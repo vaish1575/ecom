7ae0f1280a3fc5acd0018a5eed608437d2e5a311
vaish1575 <111144525+vaish1575@users.noreply.github.com>
2023-10-17 22:15:36 +0530

    this is register file

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import loginView
+from .views import loginView,registerView
 
 urlpatterns = [
     path('login/', loginView , name='login'),
+    path('register/',registerView,name='register'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,10 +1,28 @@
 
 
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login ,logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 from django.http import HttpResponse
+from .forms import CustomUserCreationForm
 
 
 # Create your views here.
 def loginView(response):
     return HttpResponse("Login page from dummy view")
+def registerView(request):
+    if request.method == 'POST':
+        form=CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            user=authenticate(username=form.cleaned_data['username'],password=form.cleaned_data['password'])
+            login(request,user)
+            # return redirect
+    else:
+        form=CustomUserCreationForm()   
+    return render(request,'user/register.html',{})
+    # return HttpResponse("Register Page From Dummy View")
